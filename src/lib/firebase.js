2a69fbf4c23f8e598b3fd272abcf79702914560a
IlijaRad <ilija.radivojevic237@gmail.com>
2021-06-18 11:28:43 +0200

    Dashboard, login, sign-up and routing

---
+++ src/lib/firebase.js
@@ -0,0 +1,22 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import {seedDatabase} from '../seed';
+
+const config = {
+    apiKey: "AIzaSyDr12L7-4lq73WB1ni3n_i3KQXdeP8ObA8",
+    authDomain: "instagram-testing-895eb.firebaseapp.com",
+    projectId: "instagram-testing-895eb",
+    storageBucket: "instagram-testing-895eb.appspot.com",
+    messagingSenderId: "609653560482",
+    appId: "1:609653560482:web:0ad14caa8f19c3d14833fb"
+};
+
+const firebase = Firebase.initializeApp(config);
+
+const {FieldValue} = Firebase.firestore;
+
+//seedDatabase(firebase);
+
+export {firebase, FieldValue};
